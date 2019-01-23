af30f38286579ec4468cb625d12eceb050ae335c
Jeff <jwatkins6@vcu.edu>
2019-01-22 21:26:16 -0500

    updating changes

--- 01_core_analysis.R
+++ 01_core_analysis.R
@@ -34,14 +34,62 @@ head(rpl.pire)
 rpl.pire$bio2013 <- a.pire * (rpl.pire$dbh_cm2013^b.pire)
 rpl.pire$bio2018 <- a.pire * (rpl.pire$dbh_cm^b.pire)
 
-head(rpl.pire)
-
 # bring back together. And you can do this with all these guys (rpl.quru etc. when you got em)
-rpl.npp <- rpl.pire
+# rpl.npp <- rpl.pire
+
+# prpe
+rpl %>%
+  filter(species == "PRPE") -> rpl.prpe
+
+head(rpl.prpe)
+# Biomass = kg (Perala and Alban, 1994 for PIRE and prpe)
+# make biomass calculations
+rpl.prpe$bio2013 <- a.prpe * (rpl.prpe$dbh_cm2013^b.prpe)
+rpl.prpe$bio2018 <- a.prpe * (rpl.prpe$dbh_cm^b.prpe)
+
+# prpe
+rpl %>%
+  filter(species == "QURU") -> rpl.quru
+
+head(rpl.quru)
+# Biomass = kg (Perala and Alban, 1994 for PIRE and QURU)
+# make biomass calculations
+rpl.quru$bio2013 <- a.quru * (rpl.quru$dbh_cm2013^b.quru)
+rpl.quru$bio2018 <- a.quru * (rpl.quru$dbh_cm^b.quru)
+
+#
+rpl.npp <- rbind(rpl.pire, rpl.prpe, rpl.quru)
 
 rpl.npp$delta.biomass <- rpl.npp$bio2018-rpl.npp$bio2013
 rpl.npp$anpp <- rpl.npp$delta.biomass / 5
+rpl.npp$rgr <- rpl.npp$anpp / rpl.npp$bio2018
 
+#sums
+rpl.npp %>%
+  group_by(plot) %>%
+  summarize(plot.npp = sum(anpp)) -> plot.npp
+
+rpl.npp %>%
+  group_by(plot, species) %>%
+  summarize(plot.rgr = mean(rgr)) -> plot.rgr
+
+
+
+plot.npp <- data.frame(plot.npp)
+
+plot.rgr <- data.frame(plot.rgr)
+
+
+
+
+
+
+
+
+
+
+
+##################
 # bring in light data
 light <- read.csv("./data/hickey_light_data.csv")
 
@@ -57,6 +105,7 @@ df$LUE <- df$anpp / df$fapar
 
 ggplot(df, aes(x = stand, y = LUE))+
   geom_boxplot()
+
 
 
 
